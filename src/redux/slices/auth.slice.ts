cafe06b1c2c3e722671be1dcf74efe45c1c15a0a
Abhijeet Mahavarkar <mahavarkar.abhi@gmail.com>
2024-12-03 15:16:39 +0530

    fixes updated eslint configs and dependencies

--- src/redux/slices/auth.slice.ts
+++ src/redux/slices/auth.slice.ts
@@ -1,13 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import { http } from '@/services/http/client'
 
-interface AuthState {
+// Define a type for the slice state
+export interface AuthState {
   user: User | null
   token: string | null
   isPending: boolean
 }
 
+// Define the initial state using that type
 const initialState: AuthState = {
   user: null,
   token: null,
@@ -18,33 +20,42 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setAuthToken(state, action) {
+    // sets auth token in the state
+    setAuthToken(state, action: PayloadAction<{ token: string }>) {
       const { payload } = action
       return {
         ...state,
         token: payload?.token,
       }
     },
-    setAuthUser(state, action) {
+
+    // sets auth user in the state
+    setAuthUser(state, action: PayloadAction<{ user: User }>) {
       const { payload } = action
       return {
         ...state,
         user: { ...state.user, ...payload.user },
       }
     },
-    setAuthPending(state, action) {
+
+    // sets auth pending in the state
+    setAuthPending(state, action: PayloadAction<{ isPending: boolean }>) {
       const { payload } = action
       return {
         ...state,
         isPending: payload.isPending || false,
       }
     },
-    rehydrateAuth(state, action) {
+
+    // rehydrates the auth state
+    rehydrateAuth(state, action: PayloadAction<AuthState>) {
       return {
         ...state,
         ...action.payload,
       }
     },
+
+    // clears the auth state
     clearAuth() {
       // Remove the token from the http client headers
       delete http.defaults.headers.common['Authorization']
--- src/redux/slices/sidebar.slice.ts
+++ src/redux/slices/sidebar.slice.ts
@@ -1,9 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-interface SidebarState {
+// Define a type for the slice state
+export interface SidebarState {
   isExpanded: boolean
 }
 
+// Define the initial state using that type
 const initialState: SidebarState = {
   isExpanded: true,
 }
@@ -12,14 +14,20 @@ const sidebarSlice = createSlice({
   name: 'sidebar',
   initialState,
   reducers: {
+    // Toggles the sidebar
     toggleSidebar: (state) => {
       state.isExpanded = !state.isExpanded
     },
+
+    // Sets the sidebar expanded
     setSidebarExpanded: (state, action: PayloadAction<boolean>) => {
       state.isExpanded = action.payload
     },
   },
 })
 
+// Export action creators as named exports
 export const { toggleSidebar, setSidebarExpanded } = sidebarSlice.actions
+
+// Export reducer as default export
 export default sidebarSlice.reducer
